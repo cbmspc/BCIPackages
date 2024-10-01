652a2acd81814594975ea01cd68f9a63aa78d8d9
ptwang <ptwang@users.noreply.github.com>
2024-10-01 14:47:33 -0700

    Updated 2024-10-01 14:44 PDT
    notchfilter: Can specify an arbitrary list of frequencies to notch. Two modes of operations: 
    1. Specifying only one frequency: Notch this frequency and its harmonics.
    2. Specifying a list of frequencies: Notch these frequencies.

--- notchfilter.m
+++ notchfilter.m
@@ -1,4 +1,4 @@
-function Signal = notchfilter (Signal, Fs, PowerLineFrequency, Order, QFactor)
+function Signal = notchfilter (Signal, Fs, NotchFreq, Order, QFactor)
 if ~exist('Order','var') || isempty(Order)
     Order = 4;
 end
@@ -6,26 +6,33 @@ if ~exist('QFactor','var') || isempty(QFactor)
     QFactor = 10;
 end
 
-Funda = PowerLineFrequency;
 persistent Hd_prev Funda_prev Order_prev QFactor_prev
 
-if iscell(Hd_prev) && Funda_prev == Funda && Order_prev == Order && QFactor_prev == QFactor
+if iscell(Hd_prev) && isequal(Funda_prev,NotchFreq) && Order_prev == Order && QFactor_prev == QFactor
     Hd = Hd_prev;
 else
-    %fprintf('Designing notch filters.\n');
-    for h = floor(Fs/2/Funda):-1:1
-        % Create one for each harmonic
-        d = fdesign.notch('N,F0,Q',Order,Funda*h/(Fs/2),QFactor*h);
-        Hd{h} = design(d);
+    if length(NotchFreq) == 1
+        % Design notch filters for NotchFreq and all of its harmonics
+        for h = floor(Fs/2/NotchFreq):-1:1
+            % Create one for each harmonic
+            d = fdesign.notch('N,F0,Q',Order,NotchFreq*h/(Fs/2),QFactor*h);
+            Hd{h} = design(d);
+        end
+        Funda_prev = NotchFreq;
+        Order_prev = Order;
+        QFactor_prev = QFactor;
+        Hd_prev = Hd;
+    else
+        % Design notch filters for the frequencies listed in NotchFreq
+        for h = length(NotchFreq):-1:1
+            % Create one for each frequency in the list
+            d = fdesign.notch('N,F0,Q',Order,NotchFreq(h)/(Fs/2),QFactor*h);
+            Hd{h} = design(d);
+        end
     end
-    Funda_prev = Funda;
-    Order_prev = Order;
-    QFactor_prev = QFactor;
-    Hd_prev = Hd;
 end
 
 for h = 1:length(Hd)
-    %fprintf('Filtering at harmonic# %i of %i.\n', h, length(Hd));
     Signal = filtfilt(Hd{h}.sosMatrix,Hd{h}.ScaleValues,double(Signal));
 end
 
