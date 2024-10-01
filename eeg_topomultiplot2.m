511528945188d9d450c80a359f8303c5c61aa98a
ptwang <ptwang@users.noreply.github.com>
2024-10-01 11:36:41 -0700

    Updated 2024-10-01 11:35 PDT
    eeg_topomultiplot2: Will omit channels with non-finite values, i.e. the channel name labels of these are hidden on the plots.

--- eeg_topomultiplot2.m
+++ eeg_topomultiplot2.m
@@ -11,9 +11,9 @@ Opts.nocontour = 1;
 Opts.npts = 64;
 
 if exist('GridDim', 'var') && length(GridDim) == 2 && size(values,2) == GridDim(1) * GridDim(2)
-    [hands, underhand] = subplotcompact(GridDim(1), GridDim(2));
+    [hands, ~] = subplotcompact(GridDim(1), GridDim(2));
 else
-    [hands, underhand] = subplotcompact(floor(sqrt(size(values,2))), ceil(sqrt(size(values,2))));
+    [hands, ~] = subplotcompact(floor(sqrt(size(values,2))), ceil(sqrt(size(values,2))));
 end
 
 if exist('OtherOpts', 'var') && isstruct(OtherOpts)
@@ -31,13 +31,20 @@ for s = 1:size(values,2)
         Opts.headcolor = headcolors(mod(s-1,size(headcolors,1))+1,:,:);
     end
 
-    eeg_topoplot2(values(:,s), ChanNames, Opts);
+    % Only pass down channels that aren't infinity or nan
+    thisvalue = values(:,s);
+    thischannames = ChanNames;
+    thisisfinite = isfinite(thisvalue);
+    thisvalue = thisvalue(thisisfinite);
+    thischannames = thischannames(thisisfinite);
+
+    eeg_topoplot2(thisvalue, thischannames, Opts);
     %title(SubplotNames{s});
     title([SubplotNames{s} '       '], 'HorizontalAlignment','right','VerticalAlignment','cap')
     pause(0.05);
 end
 
-try
+try %#ok<TRYNC>
     uicontrol(gcf, 'Style', 'text', 'Units', 'normalized', 'Position', [0.10 0.96 0.80 0.04], 'String', OtherOpts.title, 'FontUnits', 'normalized', 'FontSize', 0.6, 'Visible', 'on', 'BackgroundColor', [1 1 1], 'ForegroundColor', [0 0 0]);
     uicontrol(gcf, 'Style', 'text', 'Units', 'normalized', 'Position', [0.05 0.02 0.90 0.02], 'String', OtherOpts.footer, 'FontUnits', 'normalized', 'FontSize', 0.6, 'Visible', 'on', 'BackgroundColor', [1 1 1], 'ForegroundColor', [0 0 0]);
     %sgtitle(OtherOpts.title);
